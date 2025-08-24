af54ef7e51e9b0d15442132ba1457527a6c414bb
Seyed Mojtaba <mo93esm@gmail.com>
2025-08-24 23:58:10 +0330

    add transactions model

--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Transaction(models.Model):
+    
+    class TypeChoices(models.TextChoices):
+        Income = 'Income'
+        Expense = 'Expense'
+
+    title = models.CharField(max_length=50)
+    amount = models.models.FloatField()
+    status = models.CharField(
+        max_length=10,
+        choices=TypeChoices.choices,
+        default=StatusChoices.Expense
+    )
+    date = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    notes = models.models.CharField(max_length=100)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    
+    class Meta:
+        ordering = ['-date']
+
+        
+    def __str__(self):
+        return self.title
